f73e0364652b24c945c49d2d275a69b3ce4b4a49
Hridhyapaul <hpdesign2021@gmail.com>
2023-09-04 16:48:41 +0600

    Header section done

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Wrapper from './Wrapper';
 import Link from 'next/link';
 import Menu from './Menu';
@@ -14,19 +14,46 @@ const Header = () => {
     const [showCatMenu, setShowCatMenu] = useState(false);
     const [show, setShow] = useState("translate-y-0");
     const [lastScrollY, setLastScrollY] = useState(0);
+
+    const controlNavbar = () => {
+        if (window.scrollY > 200) {
+            if (window.scrollY > lastScrollY && !mobileMenu) {
+                setShow("-translate-y-[80px]");
+            } else {
+                setShow("shadow-sm");
+            }
+        } else {
+            setShow("translate-y-0");
+        }
+        setLastScrollY(window.scrollY);
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", controlNavbar);
+        return () => {
+            window.removeEventListener("scroll", controlNavbar);
+        };
+    }, [lastScrollY]);
+
     return (
         <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>
             <Wrapper className="h-[60px] flex justify-between items-center">
+
+                {/* <----- Logo Start -----> */}
                 <Link href="/">
                     <img src="/logo.svg" alt="" className='w-[60px] md:w-[100px]' />
                 </Link>
+                {/* <----- Logo End -----> */}
 
+                {/* <----- Start Large Screen Menu -----> */}
                 <Menu
                     showCatMenu={showCatMenu}
                     setShowCatMenu={setShowCatMenu}
                 >
                 </Menu>
+                {/* <----- End Large Screen Menu -----> */}
 
+                {/* <----- Start Mobile Screen Menu -----> */}
                 {mobileMenu && (
                     <MenuMobile
                         showCatMenu={showCatMenu}
@@ -35,23 +62,25 @@ const Header = () => {
                     >
                     </MenuMobile>
                 )}
+                {/* <----- End Mobile Screen Menu -----> */}
 
+                {/* <----- Start React & Cart Icon -----> */}
                 <div className='flex items-center gap-1 text-black'>
-                    {/* <----- Icon Start -----> */}
+                    {/* Icon Start */}
                     <div className='w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative'>
                         <IoMdHeartEmpty className='text-[19px] md:text-[24px]'></IoMdHeartEmpty>
                         <span className='h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]'>51</span>
                     </div>
-                    {/* <----- Icon End -----> */}
+                    {/* Icon End */}
 
-                    {/* <----- Icon Start -----> */}
+                    {/* Icon Start */}
                     <div className='w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative'>
                         <BsCart className='text-[15px] md:text-[20px]'></BsCart>
                         <span className='h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]'>5</span>
                     </div>
-                    {/* <----- Icon End -----> */}
+                    {/* Icon End */}
 
-                    <div className='w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2'>
+                    <div className='md:hidden w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2'>
                         {mobileMenu ? (
                             <VscChromeClose
                                 className='text-[16px]'
@@ -65,6 +94,7 @@ const Header = () => {
                         )}
                     </div>
                 </div>
+                {/* <----- End React & Cart Icon -----> */}
             </Wrapper>
         </header>
     );
