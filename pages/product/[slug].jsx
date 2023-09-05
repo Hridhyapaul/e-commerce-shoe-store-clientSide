ca9b4e931b7c66055ae09241667fb9b252fbbfac
Hridhyapaul <hpdesign2021@gmail.com>
2023-09-05 22:13:14 +0600

    Added ProductDetailsCarousel to adjust left column of product details page

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -7,7 +7,15 @@ import RelatedProducts from '@/components/RelatedProducts';
 const ProductDetails = () => {
     return (
         <div className='w-full md:py-20'>
-            
+            <Wrapper>
+                <div className='flex flex-col lg:flex-row md:px-10 gap-[50px] lg:gap-[100px]'>
+                    {/* <-----Left column start-----> */}
+                    <div className='w-full md:w-auto flex-[1.5] max-w-[500px] lg:mx-w-full mx-auto lg:mx-0'>
+                        <ProductDetailsCarousel></ProductDetailsCarousel>
+                    </div>
+                    {/* <-----Left column End-----> */}
+                </div>
+            </Wrapper>
         </div>
     );
 };
