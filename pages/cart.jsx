8a2b132e7f8324cf3fff4776a16ab79ea2b7bb58
Hridhyapaul <hpdesign2021@gmail.com>
2023-09-06 01:44:51 +0600

    Designed subtotal and checkout button in the summary column

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -6,8 +6,18 @@ import React from 'react';
 
 const Cart = () => {
     return (
-        <div>
+        <div className="w-full md:py-20">
             <Wrapper>
+
+                {/* <-----Heading and Paragraph Start-----> */}
+                <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
+                    <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+                        Shopping Cart
+                    </div>
+                </div>
+                {/* <-----Heading and Paragraph End-----> */}
+
+                {/* <-----Cart Content Start-----> */}
                 <div className='flex flex-col lg:flex-row gap-12 py-10'>
                     {/* Cart Item */}
                     <div className='flex-[2]'>
@@ -17,10 +27,35 @@ const Cart = () => {
                         <CartItem></CartItem>
                     </div>
 
+                    {/* Summary */}
                     <div className='flex-[1]'>
                         <div className='text-lg font-bold'>Summary</div>
+
+                        <div className='p-5 my-5 bg-black/[0.05] rounded-xl'>
+                            <div className='flex justify-between'>
+                                <div className='uppercase text-md md:text-lg font-medium text-black'>Subtotal</div>
+                                <div className='text-md md:text-lg font-medium text-black'>$149.99</div>
+                            </div>
+
+                            <div className="text-sm md:text-md py-5 border-t mt-5">
+                                The subtotal reflects the total price of
+                                your order, including duties and taxes,
+                                before any applicable discounts. It does
+                                not include delivery costs and
+                                international transaction fees.
+                            </div>
+                        </div>
+
+                        {/* BUTTON START */}
+                        <button
+                            className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
+                        >
+                            Checkout
+                        </button>
+                        {/* BUTTON END */}
                     </div>
                 </div>
+                {/* <-----Cart Content End-----> */}
             </Wrapper>
         </div>
     );
