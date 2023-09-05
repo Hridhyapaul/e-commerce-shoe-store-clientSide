d160c6484e0959232b171d88f6e89b59719d2c3a
Hridhyapaul <hpdesign2021@gmail.com>
2023-09-05 22:06:38 +0600

    Created dynamic route to show product details

---
+++ pages/product/[slug].jsx
@@ -0,0 +1,15 @@
+import Wrapper from '@/components/Wrapper';
+import React from 'react';
+import { IoMdHeartEmpty } from "react-icons/io";
+import ProductDetailsCarousel from '@/components/ProductDetailsCarousel';
+import RelatedProducts from '@/components/RelatedProducts';
+
+const ProductDetails = () => {
+    return (
+        <div className='w-full md:py-20'>
+            
+        </div>
+    );
+};
+
+export default ProductDetails;
